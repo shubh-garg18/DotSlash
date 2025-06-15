35d2b0d7b315ad6bb2ea77b542d561d4349e18d2
Tanishq Tyagi <tanishqtyagi@Tanishqs-MacBook-Air.local>
2025-06-15 21:03:56 +0530

    more server actions

--- server/src/server.ts
+++ server/src/server.ts
@@ -141,6 +141,131 @@ io.on("connection", (socket) => {
 			.emit(SocketEvent.DIRECTORY_DELETED, { dirId })
 	})
 
+	socket.on(SocketEvent.FILE_CREATED, ({ parentDirId, newFile }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast
+			.to(roomId)
+			.emit(SocketEvent.FILE_CREATED, { parentDirId, newFile })
+	})
+
+	socket.on(SocketEvent.FILE_UPDATED, ({ fileId, newContent }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(SocketEvent.FILE_UPDATED, {
+			fileId,
+			newContent,
+		})
+	})
+
+	socket.on(SocketEvent.FILE_RENAMED, ({ fileId, newName }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(SocketEvent.FILE_RENAMED, {
+			fileId,
+			newName,
+		})
+	})
+
+	socket.on(SocketEvent.FILE_DELETED, ({ fileId }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(SocketEvent.FILE_DELETED, { fileId })
+	})
+
+	// Handle user status
+	socket.on(SocketEvent.USER_OFFLINE, ({ socketId }) => {
+		userSocketMap = userSocketMap.map((user) => {
+			if (user.socketId === socketId) {
+				return { ...user, status: USER_CONNECTION_STATUS.OFFLINE }
+			}
+			return user
+		})
+		const roomId = getRoomId(socketId)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(SocketEvent.USER_OFFLINE, { socketId })
+	})
+
+	socket.on(SocketEvent.USER_ONLINE, ({ socketId }) => {
+		userSocketMap = userSocketMap.map((user) => {
+			if (user.socketId === socketId) {
+				return { ...user, status: USER_CONNECTION_STATUS.ONLINE }
+			}
+			return user
+		})
+		const roomId = getRoomId(socketId)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(SocketEvent.USER_ONLINE, { socketId })
+	})
+
+	// Handle chat actions
+	socket.on(SocketEvent.SEND_MESSAGE, ({ message }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast
+			.to(roomId)
+			.emit(SocketEvent.RECEIVE_MESSAGE, { message })
+	})
+
+	// Handle cursor position
+	socket.on(SocketEvent.TYPING_START, ({ cursorPosition }) => {
+		userSocketMap = userSocketMap.map((user) => {
+			if (user.socketId === socket.id) {
+				return { ...user, typing: true, cursorPosition }
+			}
+			return user
+		})
+		const user = getUserBySocketId(socket.id)
+		if (!user) return
+		const roomId = user.roomId
+		socket.broadcast.to(roomId).emit(SocketEvent.TYPING_START, { user })
+	})
+
+	socket.on(SocketEvent.TYPING_PAUSE, () => {
+		userSocketMap = userSocketMap.map((user) => {
+			if (user.socketId === socket.id) {
+				return { ...user, typing: false }
+			}
+			return user
+		})
+		const user = getUserBySocketId(socket.id)
+		if (!user) return
+		const roomId = user.roomId
+		socket.broadcast.to(roomId).emit(SocketEvent.TYPING_PAUSE, { user })
+	})
+
+	socket.on(SocketEvent.REQUEST_DRAWING, () => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast
+			.to(roomId)
+			.emit(SocketEvent.REQUEST_DRAWING, { socketId: socket.id })
+	})
+
+	socket.on(SocketEvent.SYNC_DRAWING, ({ drawingData, socketId }) => {
+		socket.broadcast
+			.to(socketId)
+			.emit(SocketEvent.SYNC_DRAWING, { drawingData })
+	})
+
+	socket.on(SocketEvent.DRAWING_UPDATE, ({ snapshot }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(SocketEvent.DRAWING_UPDATE, {
+			snapshot,
+		})
+	})
+
+	const PORT = process.env.PORT || 3000
+
+	app.get("/", (req: Request, res: Response) => {
+		// Send the index.html file
+		res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+	})
+
+	server.listen(PORT, () => {
+		console.log(`Listening on port ${PORT}`)
+	})
 
 })
 
