6550d6090e7d5041bce090885c62d41b6a74a8dd
Shubh Garg <149188565+shubh-garg18@users.noreply.github.com>
2025-06-16 19:17:08 +0530

    Updating types

--- server/src/types/socket.ts
+++ server/src/types/socket.ts
@@ -3,29 +3,29 @@ import { Socket } from "socket.io"
 type SocketId = string
 
 enum SocketEvent {
-	JOIN_REQUEST = "join-request",
+	SYNC_FILE_STRUCTURE = "sync-file-structure",
 	JOIN_ACCEPTED = "join-accepted",
 	USER_JOINED = "user-joined",
+	JOIN_REQUEST = "join-request",
 	USER_DISCONNECTED = "user-disconnected",
-	SYNC_FILE_STRUCTURE = "sync-file-structure",
 	DIRECTORY_CREATED = "directory-created",
+	DIRECTORY_DELETED = "directory-deleted",
 	DIRECTORY_UPDATED = "directory-updated",
 	DIRECTORY_RENAMED = "directory-renamed",
-	DIRECTORY_DELETED = "directory-deleted",
-	FILE_CREATED = "file-created",
-	FILE_UPDATED = "file-updated",
 	FILE_RENAMED = "file-renamed",
+	FILE_UPDATED = "file-updated",
 	FILE_DELETED = "file-deleted",
 	USER_OFFLINE = "offline",
-	USER_ONLINE = "online",
+	FILE_CREATED = "file-created",
 	SEND_MESSAGE = "send-message",
 	RECEIVE_MESSAGE = "receive-message",
+	USERNAME_EXISTS = "username-exists",
 	TYPING_START = "typing-start",
 	TYPING_PAUSE = "typing-pause",
-	USERNAME_EXISTS = "username-exists",
-	REQUEST_DRAWING = "request-drawing",
+	USER_ONLINE = "online",
 	SYNC_DRAWING = "sync-drawing",
 	DRAWING_UPDATE = "drawing-update",
+	REQUEST_DRAWING = "request-drawing",
 }
 
 interface SocketContext {
