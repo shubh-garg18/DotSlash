7889af364247912ebaa3c6aaf0af803fdb6c4874
Tanishq Tyagi <tanishqtyagi@Tanishqs-MacBook-Air.local>
2025-06-16 12:16:20 +0530

    server new functions

--- server/src/server.ts
+++ server/src/server.ts
@@ -256,16 +256,16 @@ io.on("connection", (socket) => {
 		})
 	})
 
-	const PORT = process.env.PORT || 3000
+})
 
-	app.get("/", (req: Request, res: Response) => {
-		// Send the index.html file
-		res.sendFile(path.join(__dirname, "..", "public", "index.html"))
-	})
+const PORT = process.env.PORT || 3000
 
-	server.listen(PORT, () => {
-		console.log(`Listening on port ${PORT}`)
-	})
+app.get("/", (req: Request, res: Response) => {
+	// Send the index.html file
+	res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
 
+server.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`)
 })
 
