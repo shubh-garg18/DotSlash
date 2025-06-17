66c250750718bc3ad32d73fd157a78b2d8c5b473
Shubh Garg <149188565+shubh-garg18@users.noreply.github.com>
2025-06-18 00:30:25 +0530

    Make Setting view visually better

--- client/src/components/sidebar/sidebar-views/SettingsView.tsx
+++ client/src/components/sidebar/sidebar-views/SettingsView.tsx
@@ -1,95 +1,113 @@
-import { useRunCode } from "@/context/RunCodeContext"
+import Select from "@/components/common/Select"
+import { useSettings } from "@/context/SettingContext"
 import useResponsive from "@/hooks/useResponsive"
-import { ChangeEvent } from "react"
-import toast from "react-hot-toast"
-import { LuCopy } from "react-icons/lu"
-import { PiCaretDownBold } from "react-icons/pi"
+import { editorFonts } from "@/resources/Fonts"
+import { editorThemes } from "@/resources/Themes"
+import { langNames } from "@uiw/codemirror-extensions-langs"
+import { ChangeEvent, useEffect } from "react"
 
-function RunView() {
-    const { viewHeight } = useResponsive()
+function SettingsView() {
     const {
-        setInput,
-        output,
-        isRunning,
-        supportedLanguages,
-        selectedLanguage,
-        setSelectedLanguage,
-        runCode,
-    } = useRunCode()
+        theme,
+        setTheme,
+        language,
+        setLanguage,
+        fontSize,
+        setFontSize,
+        fontFamily,
+        setFontFamily,
+        showGitHubCorner,
+        setShowGitHubCorner,
+        resetSettings,
+    } = useSettings()
+    const { viewHeight } = useResponsive()
 
-    const handleLanguageChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        const lang = JSON.parse(e.target.value)
-        setSelectedLanguage(lang)
-    }
+    const handleFontFamilyChange = (e: ChangeEvent<HTMLSelectElement>) =>
+        setFontFamily(e.target.value)
+    const handleThemeChange = (e: ChangeEvent<HTMLSelectElement>) =>
+        setTheme(e.target.value)
+    const handleLanguageChange = (e: ChangeEvent<HTMLSelectElement>) =>
+        setLanguage(e.target.value)
+    const handleFontSizeChange = (e: ChangeEvent<HTMLSelectElement>) =>
+        setFontSize(parseInt(e.target.value))
+    const handleShowGitHubCornerChange = (e: ChangeEvent<HTMLInputElement>) =>
+        setShowGitHubCorner(e.target.checked)
 
-    const copyOutput = () => {
-        navigator.clipboard.writeText(output)
-        toast.success("Output copied to clipboard")
-    }
+    useEffect(() => {
+        // Set editor font family
+        const editor = document.querySelector(
+            ".cm-editor > .cm-scroller",
+        ) as HTMLElement
+        if (editor !== null) {
+            editor.style.fontFamily = ${fontFamily}, monospace
+        }
+    }, [fontFamily])
 
     return (
         <div
             className="flex flex-col items-center gap-2 p-4"
             style={{ height: viewHeight }}
         >
-            <h1 className="view-title">Run Code</h1>
-            <div className="flex h-[90%] w-full flex-col items-end gap-2 md:h-[92%]">
-                <div className="relative w-full">
-                    <select
-                        className="w-full rounded-md border-none bg-darkHover px-4 py-2 text-white outline-none"
-                        value={JSON.stringify(selectedLanguage)}
-                        onChange={handleLanguageChange}
-                    >
-                        {supportedLanguages
-                            .sort((a, b) => (a.language > b.language ? 1 : -1))
-                            .map((lang, i) => {
-                                return (
-                                    <option
-                                        key={i}
-                                        value={JSON.stringify(lang)}
-                                    >
-                                        {lang.language +
-                                            (lang.version
-                                                ? ` (${lang.version})`
-                                                : "")}
-                                    </option>
-                                )
-                            })}
-                    </select>
-                    <PiCaretDownBold
-                        size={16}
-                        className="absolute bottom-3 right-4 z-10 text-white"
-                    />
-                </div>
-                <textarea
-                    className="min-h-[120px] w-full resize-none rounded-md border-none bg-darkHover p-2 text-white outline-none"
-                    placeholder="Write you input here..."
-                    onChange={(e) => setInput(e.target.value)}
+            <h1 className="view-title">Settings</h1>
+            {/* Choose Font Family option */}
+            <div className="flex w-full items-end gap-2">
+                <Select
+                    onChange={handleFontFamilyChange}
+                    value={fontFamily}
+                    options={editorFonts}
+                    title="Font Family"
                 />
-                <button
-                    className="flex w-full justify-center rounded-md bg-primary p-2 font-bold text-black outline-none disabled:cursor-not-allowed disabled:opacity-50"
-                    onClick={runCode}
-                    disabled={isRunning}
+                {/* Choose font size option */}
+                <select
+                    value={fontSize}
+                    onChange={handleFontSizeChange}
+                    className="rounded-md border-none bg-darkHover px-4 py-2 text-white outline-none"
+                    title="Font Size"
                 >
-                    Run
-                </button>
-                <label className="flex w-full justify-between">
-                    Output :
-                    <button onClick={copyOutput} title="Copy Output">
-                        <LuCopy
-                            size={18}
-                            className="cursor-pointer text-white"
-                        />
-                    </button>
-                </label>
-                <div className="w-full flex-grow resize-none overflow-y-auto rounded-md border-none bg-darkHover p-2 text-white outline-none">
-                    <code>
-                        <pre className="text-wrap">{output}</pre>
-                    </code>
-                </div>
+                    {[...Array(13).keys()].map((size) => {
+                        return (
+                            <option key={size} value={size + 12}>
+                                {size + 12}
+                            </option>
+                        )
+                    })}
+                </select>
             </div>
+            {/* Choose theme option */}
+            <Select
+                onChange={handleThemeChange}
+                value={theme}
+                options={Object.keys(editorThemes)}
+                title="Theme"
+            />
+            {/* Choose language option */}
+            <Select
+                onChange={handleLanguageChange}
+                value={language}
+                options={langNames}
+                title="Language"
+            />
+            {/* Show GitHub corner option */}
+            <div className="mt-4 flex w-full items-center justify-between">
+                <label>Show github corner</label>
+                <label className="relative inline-flex cursor-pointer items-center">
+                    <input
+                        className="peer sr-only"
+                        type="checkbox"
+                        onChange={handleShowGitHubCornerChange}
+                        checked={showGitHubCorner}
+                    />
+                    <div className="peer h-6 w-12 rounded-full bg-darkHover outline-none duration-100 after:absolute after:left-1 after:top-1 after:flex after:h-4 after:w-4 after:items-center after:justify-center after:rounded-full after:bg-white after:font-bold after:outline-none after:duration-500 peer-checked:after:translate-x-6 peer-checked:after:border-white peer-focus:outline-none"></div>
+                </label>
+            </div>
+            <button
+                className="mt-auto w-full rounded-md border-none bg-darkHover px-4 py-2 text-white outline-none"
+                onClick={resetSettings}
+            >
+                Reset to default
+            </button>
         </div>
     )
 }
 
-export default RunView
+export default SettingsView
