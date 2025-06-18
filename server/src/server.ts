675828d7f71b15e992fbee25dc060a528237e4d9
Tanishq Tyagi <tanishqtyagi@Tanishqs-MacBook-Air.local>
2025-06-18 20:40:36 +0530

    cors error

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,7 +19,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
 	cors: {
-		origin: "https://dot-slash-oigb.vercel.app",
+		origin: "*",
 	},
 	maxHttpBufferSize: 1e8,
 	pingTimeout: 60000,
